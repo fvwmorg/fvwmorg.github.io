f8681fd6659e22058b47e845023e8275526c9f0a
uwp <uwp>
2003-03-31 07:40:28 +0000

    Added php files to get information about php-module on fvwm.org before commiting the new web page.

---
+++ php_info/php_info.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>php info</title>
+  </head>
+
+  <body>
+    <h1>php info</h1>
+
+    <?php phpinfo(); ?>
+
+  </body>
+</html>
