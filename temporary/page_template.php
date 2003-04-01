ff812de8308a1886cffbfcf61d2fffefb1e1ab5d
uwp <uwp>
2003-04-01 13:43:10 +0000

    added template files for development

---
+++ temporary/page_template.php
@@ -0,0 +1,48 @@
+<?php
+//--------------------------------------------------------------------
+//-  File          : @FILENAME@
+//-  Project       : Fvwm Home page
+//-  Programmer    : Uwe Pross
+//-  Last modified : <26.03.2003 22:32:52 uwe>
+//--------------------------------------------------------------------
+
+if( strlen($rel_path)==0 ) $rel_path="@REL_PATH@";
+
+//--------------------------------------------------------------------
+// load some global definitions
+//--------------------------------------------------------------------
+include($rel_path.'/definitions.inc'); 
+
+//--------------------------------------------------------------------
+// Site definitions
+//--------------------------------------------------------------------
+$title          = "FVWM Sub site - @FILENAME@";
+$heading        = "FVWM Sub site - @FILENAME@";
+$link_name      = "@FILENAME@";
+$link_picture   = "pictures/icons/default";
+$parent_site    = "@PARENT_SITE@";
+$child_sites    = array();
+$requested_file = basename(my_get_global("PHP_SELF", "SERVER"));
+$this_site      = str_replace(".php","","$requested_file");
+
+//--------------------------------------------------------------------
+// check if we should stop here
+//--------------------------------------------------------------------
+if( strlen("$navigation_check") > 0 ) return;
+
+//--------------------------------------------------------------------
+// load the layout file
+//--------------------------------------------------------------------
+if( strlen("$site_has_been_loaded") == 0 ) {
+  $site_has_been_loaded = "true";
+  if( strlen($layout) > 0 ) {
+    $file = $rel_path."/layout_".$layout.".inc";
+    if( file_exists($file) ) $layout_file = $file;
+  }
+  include(sec_filename("$layout_file"));
+  exit();
+}
+?>
+
+-- site contents go here --
+
