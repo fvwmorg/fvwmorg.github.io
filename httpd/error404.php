fe8cf0bdd2681a27ae5085c3f3ba11b2be03fe5f
uwp <uwp>
2003-04-21 20:00:14 +0000

    Added map function to error404.php. Some comments.

--- httpd/error404.php
+++ httpd/error404.php
@@ -1,15 +1,56 @@
 <?php
 
-// We may place a map function here 
-// e.g. download.html -> download/index.php
+//--------------------------------------------------------------------
+// find the best link to the failed request
+//--------------------------------------------------------------------
+if( strpos($request,"creating_rpms") )             $file = "/documentation/dev_creating_rpms.php";
+elseif( strpos($request,"cvs") )                   $file = "/documentation/dev_cvs.php";
+elseif( strpos($request,"developers") )            $file = "/documentation/developers.php";
+elseif( strpos($request,"manpages") )              $file = "/documentation/manpages/index.php";
+// elseif( strpos($request,"download/map_mini") ) $file = "/";
+elseif( strpos($request,"download") )              $file = "/download/index.php";
+elseif( strpos($request,"downloadbugs") )          $file = "/documentation/dev_downloadbugs.php";
+elseif( strpos($request,"features") )              $file = "/features.php";
+elseif( strpos($request,"ftp") )                   $file = "/contact/index.php";
+elseif( strpos($request,"fvwm-cats/index") )       $file = "/fvwm-cats/index.php";
+elseif( strpos($request,"generated/AUTHORS") )     $file = "/authors/index.php";
+elseif( strpos($request,"generated/FAQ") )         $file = "/documentation/faq.php";
+elseif( strpos($request,"generated/NEWS") )        $file = "/latest_news/index.php";
+elseif( strpos($request,"perllib") )               $file = "/documentation/perllib/index.php";
+elseif( strpos($request,"icons") )                 $file = "/download/icons.php";
+elseif( strpos($request,"links") )                 $file = "/links.php";
+elseif( strpos($request,"mailinglist") )           $file = "/contact/index.php";
+elseif( strpos($request,"mod_changes") )           $file = "/documentation/dev_modules.php";
+elseif( strpos($request,"mod_concept") )           $file = "/documentation/dev_modules.php";
+elseif( strpos($request,"mod_f2m_communication") ) $file = "/documentation/dev_modules.php";
+elseif( strpos($request,"mod_initialization") )    $file = "/documentation/dev_modules.php";
+elseif( strpos($request,"mod_m2f_communication") ) $file = "/documentation/dev_modules.php";
+elseif( strpos($request,"mod_security") )          $file = "/documentation/dev_modules.php";
+elseif( strpos($request,"modules") )               $file = "/documentation/dev_modules.php";
+elseif( strpos($request,"sample-menus/index") )    $file = "/screenshots/menus/index.php";
+elseif( strpos($request,"screenshots") )           $file = "/screenshots/index.php";
+elseif( strpos($request,"vector-buttons") )        $file = "/screenshots/windowdecors/index.php";
+// elseif( strpos($request,"xxxacknowledgements") ) $file = "/";
+else $file = "/index.php";
 
-?>
+if( file_exists("..".$file) ) {
+  $url = "http://".$_SERVER["HTTP_HOST"].$file;
+} else {
+  $url = "http://".$_SERVER["HTTP_HOST"];
+}
+
+//--------------------------------------------------------------------
+//- Set http header to error 404
+//--------------------------------------------------------------------
+header("HTTP/1.0 404 Not Found");
+
+?> 
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <html>
   <head>
-    <meta http-equiv="refresh" content="5; URL=http://<?php echo $_SERVER['HTTP_HOST'];?>/">
+    <meta http-equiv="refresh" content="5; URL=<?php echo $url;?>">
     <title>Document is not on this server - Error 404</title>
   </head>
 
@@ -18,12 +59,12 @@
 
     <p>
       You request for document 
-      <b><?php echo $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];?></b>
+      <b><?php echo "http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];?></b>
       could be not fulfilled.
     </p>
 
     <p>
-      Please update your bookmarks to <a href="../"><?php echo $_SERVER["HTTP_HOST"];?></a>
+      Please update your bookmarks to <a href="<?php echo $url;?>"><?php echo $url;?></a>
     </p>
     
     <p>
