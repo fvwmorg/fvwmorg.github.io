9b4395fde8877165c7a7f440d921593b87349c68
uwp <uwp>
2003-05-16 21:27:16 +0000

    Forgotten file

---
+++ screenshots/An_Thi_Nguyen_Le-desk3-1152x864.php
@@ -0,0 +1,11 @@
+<?php if( strlen("$navigation_check") > 0 ) return; ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>An_Thi_Nguyen_Le-desk3-1152x864</title>
+  </head>
+
+  <body bgcolor="#000000" color="#dddddd">
+    <a href="javascript:self.close()"><img src="An_Thi_Nguyen_Le-desk3-1152x864.gif"></a>
+  </body>
+</html>
