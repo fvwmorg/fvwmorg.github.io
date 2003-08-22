3dd71342d794167d502a53805239b4a0ecf4a9ff
uwp <uwp>
2003-08-22 12:57:29 +0000

    Added logo from Tom Krietemeyer

---
+++ logo-competition/logos/Tom_Krietemeyer/index.php
@@ -0,0 +1,57 @@
+<?php
+//--------------------------------------------------------------------
+//-  File          : @FILENAME@
+//-  Project       : FVWM Home page
+//-  Programmer    : Logo Competitor
+//--------------------------------------------------------------------
+
+//--------------------------------------------------------------------
+// check if we should stop here
+//--------------------------------------------------------------------
+if (strlen($navigation_check) > 0) return;
+
+$rel_path = "../../..";
+
+//--------------------------------------------------------------------
+// load some
+//--------------------------------------------------------------------
+include($rel_path."/definitions.inc");
+
+//--------------------------------------------------------------------
+// Site definitions
+//--------------------------------------------------------------------
+$title          = "FVWM - Logo Competition - Tom W Krietemeyer logos";
+$heading        = "FVWM - Logo Competition - Tom W Krietemeyer logos";
+$link_name      = "Logo Competition";
+$link_picture   = "pictures/icons/logo_competition";
+$parent_site    = "news";
+$child_sites    = array();
+$requested_file = basename(my_get_global("PHP_SELF", "SERVER"));
+$this_site      = "logo_competition";
+
+//--------------------------------------------------------------------
+// load the layout file
+//--------------------------------------------------------------------
+if (strlen($site_has_been_loaded) == 0) {
+	$site_has_been_loaded = "true";
+	include(sec_filename($layout_file));
+	exit();
+}
+
+if( file_exists("./../color_select.inc") ) include("./../color_select.inc");
+?>
+
+<?php decoration_window_start("By Tom W Krietemeyer", "100%", ""); ?>
+
+<p>
+<?php
+// From: "Krietemeyer, Tom W" <TKrietem@usi.edu>                                                                                     
+// Date: Wed, 20 Aug 2003 09:07:10 -0500                                                                                             
+// To: fvwm-logo@lists.sourceforge.net                                                                                               
+// Subject: [FVWM-LOGO] (no subject)                                                                                                 
+?>
+<p><img src="fvwm-logo.png"></p>
+
+<?php decoration_window_end(); ?>
+
+<p>Return to <a href="<?php echo conv_link_target('../../logos_new.php');?>">index</a>.</p>
--- logo-competition/logos_new.php
+++ logo-competition/logos_new.php
@@ -480,4 +480,17 @@ width="453" height="157" border="0" vspace="0" hspace="0">
 
 <!-- ============================================================= -->
 
+<?php decoration_window_start("By Tom W Krietemeyer (1 image)", "", "", 0); ?>
+
+<a href="<?php echo conv_link_target('logos/Tom_Krietemeyer/index.php'); ?>">
+
+<p><img src="logos/Tom_Krietemeyer/overview.png" border="0" vspace="0" hspace="0">
+
+<p>More...
+</a>
+
+<?php decoration_window_end(); ?>
+
+<!-- ============================================================= -->
+
 <p>Return to <a href="<?php echo conv_link_target('./');?>">index</a>.</p>
