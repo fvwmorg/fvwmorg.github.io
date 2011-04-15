5eff3e297496f4ac077eff9ef4fc328898966f12
Thomas Adam <thomas@fvwm.org>
2011-04-15 13:34:23 +0000

    Mention NEWS.

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ and support is excellent.  Check it out!
 // -- begin news section
 //--------------------------------------------------------------------
 ?>
-    <b>News:</b> Improved support of 64-bit platforms in fvwm-2.5.16.
+    <b>News:</b> fvwm 2.6.0 has been released.
 <hr>
 <?php
 //--------------------------------------------------------------------
