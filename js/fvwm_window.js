7252e3809a826b373eaf45536fd0729523a97372
somiaj <jaimosskriletz@boisestate.edu>
2016-04-06 23:01:29 -0600

    Title-window shade and stick states now persistant   across a window/browser session.

--- js/fvwm_window.js
+++ js/fvwm_window.js
@@ -1,17 +1,33 @@
 function FvwmWindowShade(state) {
 	if (state) {
 		document.getElementById('window-content').style.display='none';
-		document.getElementsByTagName('body')[0].style.paddingTop='120px';
+		document.getElementsByTagName('body')[0].style.paddingTop='116px';
+		window.localStorage.setItem('FvwmWindowShade', true);
 	} else {
 		document.getElementById('window-content').style.display='block';
 		document.getElementsByTagName('body')[0].style.paddingTop='205px';
+		window.localStorage.setItem('FvwmWindowShade', false);
 	}
 }
 
 function FvwmWindowStick() {
 	if ( document.getElementById('fvwm_header').style.position == 'fixed' ) {
 		document.getElementById('fvwm_header').style.position='absolute';
+		window.localStorage.setItem('FvwmWindowStick', false);
 	} else {
 		document.getElementById('fvwm_header').style.position='fixed';
+                window.localStorage.setItem('FvwmWindowStick', true);
+	}
+}
+
+function FvwmStartFunction() {
+	shade = window.localStorage.getItem('FvwmWindowShade');
+	stick = window.localStorage.getItem('FvwmWindowStick');
+	if ( stick == 'true' ) {
+		document.getElementById('fvwm_header').style.position='fixed';
+	}
+
+	if ( shade == 'true' ) {
+		FvwmWindowShade(true);
 	}
 }
